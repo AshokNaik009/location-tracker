416e77c4278d44a820a89a13be7ea2f8b8826834
ashok <ashok.quickdesks@gmail.com>
2021-01-24 13:01:41 +0530

    Completed the signup functionality and clearErrorMessage event

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,47 +1,54 @@
 import createDataContext from "./createDataContext";
 import trackerApi from "../api/tracker";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { navigate  } from '../navigationRef';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { navigate } from "../navigationRef";
 
 const authReducer = (state, action) => {
   switch (action.type) {
-    case 'add_error': 
-      return {...state,errorMessage:action.payload };
-    case 'signup': 
-      return {errorMessage:'',token:action.payload};
+    case "add_error":
+      return { ...state, errorMessage: action.payload };
+    case "signin":
+      return { errorMessage: "", token: action.payload };
+    case "clear_error_message":
+      return { ...state,errorMessage:""};
     default:
       return state;
   }
 };
 
-
-const signup = dispatch => async ({ email, password }) => {
-    try {
-      const response = await trackerApi.post('/signup', { email, password });
-      await AsyncStorage.setItem('token', response.data.token);
-      dispatch({type:'signup',payload:response.data.token});
-      navigate('TrackList')
-    } catch (err) {
-      dispatch({type:'add_error', payload:'Something went wrong'});
-      //console.log(err.response.data);
-    }
-  };
-
-
-const signin = (dispatch) =>{
-  return({email,password}) =>{
-      //Api request
-  }
+const clearErrorMessage =  dispatch => () =>{
+  dispatch({type:'clear_error_message'})
 }
 
-const signout = (dispatch) =>{
-  return() =>{
-
+const signup = (dispatch) => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post("/signup", { email, password });
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch({ type: "signin", payload: response.data.token });
+    navigate("TrackList");
+  } catch (err) {
+    dispatch({ type: "add_error", payload: "Something went wrong" });
+    //console.log(err.response.data);
   }
-}
+};
+
+const signin = (dispatch) => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post("/signin", { email, password });
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch({ type: "signin", payload: response.data.token });
+    navigate("TrackList");
+  } catch (error) {
+    dispatch({ type: "add_error", payload: "Something went wrong" });
+  }
+};
+
+const signout = (dispatch) => {
+  return () => {};
+};
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {signin,signup,signout},
-  { token: null ,errorMessage:'' }
+  { signin, signup, signout ,clearErrorMessage },
+  { token: null, errorMessage: "" }
 );
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,12 +1,38 @@
-import React from "react";
-import { Text , View ,StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import AuthForm from "./components/AuthForm";
+import NavLink from "./components/NavLink";
+import {Context }from "../context/AuthContext";
+import { NavigationEvents } from "react-navigation"; 
 
-const SigninScreen = () =>{
-    return( <View>
-          <Text style={{fontSize:48}}>  SigninScreen </Text>
-    </View>);
-}
+const SigninScreen = () => {
+  const { state, signin , clearErrorMessage } = useContext(Context);
+  return (
+    <View style={styles.container}>
+        <NavigationEvents onWillFocus={clearErrorMessage} />
+      <AuthForm
+        headerText="Sign in for Tracker"
+        errorMessage={state.errorMessage}
+        onSubmit={signin}
+        submitButtonText="Sign In"
+      />
+      <NavLink text="Dont have an account? Signup instead" routeName="Signup" />
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+});
 
 export default SigninScreen;
