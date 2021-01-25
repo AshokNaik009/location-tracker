f2cdbaea0f1a30305ff41ddcf22dcb9afab3be89
ashok <ashok.quickdesks@gmail.com>
2021-01-25 11:38:23 +0530

    Added logout feature in the AccountScreen component

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext ,useEffect  } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { NavigationEvents } from "react-navigation"; 
 import { Context as AuthContext } from "../context/AuthContext";
 import AuthForm from "./components/AuthForm";
 import NavLink from "./components/NavLink";
 const SignupScreen = ({ navigation }) => {
-  const { state, signup ,clearErrorMessage } = useContext(AuthContext);
+  const { state, signup ,clearErrorMessage ,tryLocalSignin } = useContext(AuthContext);
+  useEffect(()=>{
+    tryLocalSignin();
+  },[])
   return (
     <View style={styles.container}>
       <NavigationEvents onWillFocus={clearErrorMessage} />
