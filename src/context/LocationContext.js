ffd8dfd635a4bec1b93b6666871147432bc3b4e1
ashok <ashok.quickdesks@gmail.com>
2021-02-05 12:53:12 +0530

    Integrated Add Tracks Api and Added  Reset form action

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -12,6 +12,8 @@ const locationReducer = (state, action) => {
       return { ...state, locations: [...state.locations, action.payload] };
     case 'change_name':
       return { ...state, name: action.payload };
+     case 'reset':
+        return { ...state, name: '' ,locations:[] };
     default:
       return state;
   }
@@ -26,6 +28,9 @@ const startRecording = dispatch => () => {
 const stopRecording = dispatch => () => {
   dispatch({ type: 'stop_recording' });
 };
+const reset = dispatch => () =>{
+  dispatch({ type: 'reset' });
+}
 const addLocation = dispatch => (location, recording) => {
   dispatch({ type: 'add_current_location', payload: location });
   if (recording) {
@@ -35,6 +40,6 @@ const addLocation = dispatch => (location, recording) => {
 
 export const { Context, Provider } = createDataContext(
   locationReducer,
-  { startRecording, stopRecording, addLocation, changeName },
+  { startRecording, stopRecording, addLocation, changeName ,reset },
   { name: '', recording: false, locations: [], currentLocation: null }
 );
